5c1d627e3c76bed0a9ca6846822ce6d9a13e92a4
Cody Balos <balos1@llnl.gov>
2025-12-30 15:20:10 -0800

    address comments on sundials4py_helpers.hpp

--- bindings/sundials4py/include/sundials4py_helpers.hpp
+++ bindings/sundials4py/include/sundials4py_helpers.hpp
@@ -27,11 +27,13 @@ namespace sundials4py {
 /// This function will call a user-supplied Python function through C++ side wrappers
 /// \tparam FnType is the function signature, e.g., std::remove_pointer_t<CVRhsFn>
 /// \tparam FnTableType is the struct function table that holds the user-supplied Python functions as std::function
-/// \tparam UserDataArg is the index of the void* user_data argument of the C function. We are counting from the last arg to the first arg, so if user_data is the last arg then this should be 1.
+/// \tparam UserDataArg is the index of the void* user_data argument of the C function.
+///         We are counting from the last arg to the first arg, so if user_data is the last arg then this should be 1.
 /// \tparam Args is the template parameter pack that contains all of the types of the function arguments to the C function
 ///
 /// \param fn_member is the name of the function in the FnTableType to call
-/// \param args is the arguments to the C function, which will be forwarded to the user-supplied Python function, except user_data, which is intercepted and passed as a nullptr.
+/// \param args is the arguments to the C function, which will be forwarded to the user-supplied Python function,
+///        except user_data, which is intercepted and passed as a nullptr.
 template<typename FnType, typename FnTableType, std::size_t UserDataArg, typename... Args>
 int user_supplied_fn_caller(nb::object FnTableType::*fn_member, Args... args)
 {
@@ -56,18 +58,22 @@ int user_supplied_fn_caller(nb::object FnTableType::*fn_member, Args... args)
 /// \tparam FnType is the function signature, e.g., std::remove_pointer_t<CVRhsFn>
 /// \tparam FnTableType is the struct function table that holds the user-supplied Python functions as std::function
 /// \tparam MemType the type that user_data will be cast to
-/// \tparam UserDataArg is the index of the void* user_data argument of the C function. We are counting from the last arg to the first arg, so if user_data is the last arg then this should be 1.
-/// \tparam Args is the template parameter pack that contains all of the types of the function arguments to the C function
+/// \tparam UserDataArg is the index of the void* user_data argument of the C function.
+///         We are counting from the last arg to the first arg, so if user_data is the last arg then this should be 1.
+/// \tparam Args is the template parameter pack that contains all of the types of the
+///         function arguments to the C function
 ///
 /// \param fn_member is the name of the function in the FnTableType to call
-/// \param args is the arguments to the C function, which will be forwarded to the user-supplied Python function, except user_data, which is intercepted and passed as a nullptr.
+/// \param args is the arguments to the C function, which will be forwarded to the user-supplied
+///        Python function, except user_data, which is intercepted and passed as a nullptr.
 template<typename FnType, typename FnTableType, typename MemType,
          std::size_t UserDataArg, typename... Args>
 int user_supplied_fn_caller(nb::object FnTableType::*fn_member, Args... args)
 {
-  constexpr size_t N            = sizeof...(Args);
-  constexpr int user_data_index = N - UserDataArg;
-  auto args_tuple               = std::tuple<Args...>(args...);
+  constexpr size_t N = sizeof...(Args);
+  static_assert(UserDataArg >= 1 && UserDataArg <= N);
+  constexpr size_t user_data_index = N - UserDataArg;
+  auto args_tuple                  = std::tuple<Args...>(args...);
 
   // Extract user_data from the specified position
   void* user_data = std::get<user_data_index>(args_tuple);
@@ -77,29 +83,12 @@ int user_supplied_fn_caller(nb::object FnTableType::*fn_member, Args... args)
   auto fn_table = static_cast<FnTableType*>(mem->python);
   auto fn       = nb::cast<std::function<FnType>>(fn_table->*fn_member);
 
-  // Pass nullptr as user_data since we do not want the user to mess with user_data (which holds our function table)
+  // Pass nullptr as user_data since we do not want the user to mess with
+  // user_data (which holds our function table)
+  static_assert(
+    std::is_same_v<std::tuple_element_t<user_data_index, decltype(args_tuple)>,
+                   void*>);
   std::get<user_data_index>(args_tuple) = nullptr;
-  return std::apply([&](auto&&... call_args) { return fn(call_args...); },
-                    args_tuple);
-}
-
-/// This function will call a user-supplied Python function through C++ side wrappers
-/// \tparam FnType is the function signature, e.g., std::remove_pointer_t<CVRhsFn>
-/// \tparam FnTableType is the struct function table that holds the user-supplied Python functions as std::function
-/// \tparam Args is the template parameter pack that contains all of the types of the function arguments to the C function
-///
-/// \param fn_member is the name of the function in the FnTableType to call
-/// \param args is the arguments to the C function, which will be forwarded to the user-supplied Python function.
-template<typename FnType, typename FnTableType, typename... Args>
-int user_supplied_fn_caller(nb::object FnTableType::*fn_member, void* user_data,
-                            Args... args)
-{
-  auto args_tuple = std::tuple<Args...>(args...);
-
-  // Cast user_data to FnTableType*
-  auto fn_table = static_cast<FnTableType*>(user_data);
-  auto fn       = nb::cast<std::function<FnType>>(fn_table->*fn_member);
-
   return std::apply([&](auto&&... call_args) { return fn(call_args...); },
                     args_tuple);
 }
@@ -132,8 +121,11 @@ int user_supplied_fn_caller(nb::object FnTableType::*fn_member, Args... args)
 /// elements of the sequence (e.g., a tuple) are Nurses.
 ///
 /// \tparam IP Index of the input argument who is kept alive by the nurses.
+///         The range of IP is [0, num_args] where the 0 is the return value and 1...num_args
+///         are the function input arguments in natural order.
 /// \tparam IN Indexes of the return values in the returned sequence who keep the patient alive.
-//
+///         The range of IN is [0,num_tuple_entries) where the 0 is the 0th entry in the tuple.
+///
 /// See https://nanobind.readthedocs.io/en/latest/api_core.html#_CPPv4I0EN8nanobind11call_policyE.
 ///
 template<size_t IP, size_t... IN>

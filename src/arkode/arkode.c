3615e6d3f067de3a39160442fc48af31a2838937
Daniel R. Reynolds <dreynolds@umbc.edu>
2026-01-13 22:28:54 -0500

    Removed extraneous copy of yn into yout in ONE_STEP mode (since ycur aliases yout, and just prior to this copy ycur was copied into yn)

--- src/arkode/arkode.c
+++ src/arkode/arkode.c
@@ -1133,7 +1133,6 @@ int ARKodeEvolve(void* arkode_mem, sunrealtype tout, N_Vector yout,
     {
       istate            = ARK_SUCCESS;
       ark_mem->tretlast = *tret = ark_mem->tcur;
-      N_VScale(ONE, ark_mem->yn, yout);
       ark_mem->next_h = ark_mem->hprime;
       break;
     }

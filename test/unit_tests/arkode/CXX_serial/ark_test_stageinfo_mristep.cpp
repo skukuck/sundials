29c3beaf5e0bf735c29bfaee1064d2e026d09040
Daniel R. Reynolds <dreynolds@umbc.edu>
2026-01-30 12:44:09 -0500

    Fixed approach for various 'stageinfo' unit tests to share the stageinfo.hpp header

--- test/unit_tests/arkode/CXX_serial/ark_test_stageinfo_mristep.cpp
+++ test/unit_tests/arkode/CXX_serial/ark_test_stageinfo_mristep.cpp
@@ -35,6 +35,7 @@
 
 #include "problems/kpr.hpp"
 #include "utilities/check_return.hpp"
+#include "stageinfo.hpp"
 
 using namespace std;
 using namespace problems::kpr;
@@ -44,9 +45,6 @@ using namespace problems::kpr;
 // This would normally be stored in user_data, but here we reuse problem
 // definitions from other tests.
 void* arkode_mem = nullptr;
-
-// Include the pre/post step and stage processing routines now that arkode_mem is defined
-#include "stageinfo.hpp"
 
 int main(int argc, char* argv[])
 {

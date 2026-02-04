6b84de24df9b138312c078f7a42fb922e6392abb
David J. Gardner <gardner48@llnl.gov>
2026-02-04 14:41:04 -0800

    WIP updating error estimate plots

--- test/dev_tests/arkode/stiffness/plot_shampine.py
+++ test/dev_tests/arkode/stiffness/plot_shampine.py
@@ -6,6 +6,8 @@ import matplotlib.pyplot as plt
 import os
 import sys
 
+from matplotlib.lines import Line2D
+
 # Location of suntools directory
 sys.path.append(os.path.join(os.environ['SUNDIALS_REPO'], "tools"))
 from suntools import logs as sunlog
@@ -66,6 +68,14 @@ _, times_s, vals_s = sunlog.get_history(log, 'h', "success")
 _, times_etf, vals_etf = sunlog.get_history(log, 'h', "failed error test")
 _, times_sf, vals_sf = sunlog.get_history(log, 'h', "failed solve")
 
+_, times_1a, vals_1a = sunlog.get_history(log, 'compute-solution.err(:)')
+_, times_1s, vals_1s = sunlog.get_history(log, 'compute-solution.err(:)', "success")
+_, times_1f, vals_1f = sunlog.get_history(log, 'compute-solution.err(:)', "failed")
+
+_, times_2a, vals_2a = sunlog.get_history(log, 'compute-solution.err2(:)')
+_, times_2s, vals_2s = sunlog.get_history(log, 'compute-solution.err2(:)', "success")
+_, times_2f, vals_2f = sunlog.get_history(log, 'compute-solution.err2(:)', "failed")
+
 stiffness_ratio = data["stiffness_ratio"][0]
 lambda_1 = data["lambda1"][0]
 lambda_3 = data["lambda3"][0]
@@ -95,22 +105,112 @@ if args.title:
 else:
     axes[0].set_title(title)
 axes[0].grid(True, which="both", linestyle=":", alpha=0.5)
-
 clip = np.finfo(np.float64).eps
-axes[1].plot(data["t"], np.clip(np.abs(data["loc err est1"]), clip, None), label="y1")
-axes[1].plot(data["t"], np.clip(np.abs(data["loc err est2"]), clip, None), label="y2")
-axes[1].plot(data["t"], np.clip(np.abs(data["loc err est3"]), clip, None), label="y3")
-axes[1].plot(data["t"], np.clip(np.abs(data["com loc err est1"]), clip, None), linestyle="--", label="x1")
-axes[1].plot(data["t"], np.clip(np.abs(data["com loc err est2"]), clip, None), linestyle="--", label="x2")
-axes[1].plot(data["t"], np.clip(np.abs(data["com loc err est3"]), clip, None), linestyle="--", label="x3")
+# axes[1].plot(data["t"], np.clip(np.abs(data["loc err est1"]), clip, None), label="y1")
+# axes[1].plot(data["t"], np.clip(np.abs(data["loc err est2"]), clip, None), label="y2")
+# axes[1].plot(data["t"], np.clip(np.abs(data["loc err est3"]), clip, None), label="y3")
+# axes[1].plot(data["t"], np.clip(np.abs(data["com loc err est1"]), clip, None), linestyle="--", label="x1")
+# axes[1].plot(data["t"], np.clip(np.abs(data["com loc err est2"]), clip, None), linestyle="--", label="x2")
+# axes[1].plot(data["t"], np.clip(np.abs(data["com loc err est3"]), clip, None), linestyle="--", label="x3")
+
+# data = np.array(vals_1s)
+# for i in range(data.shape[1]):
+#     axes[1].scatter(times_1s, np.abs(data[:, i]), marker='o', color=colors[i], label=f'Component {i}')
+# data = np.array(vals_1f)
+# for i in range(data.shape[1]):
+#     axes[1].scatter(times_1f, np.abs(data[:, i]), marker='x', color=colors[i], label=f'Component {i}')
+
+# data = np.array(vals_2s)
+# for i in range(data.shape[1]):
+#     axes[1].scatter(times_2s, np.abs(data[:, i]), marker='o', facecolors='none', edgecolors=colors[i], label=f'Component {i}')
+# data = np.array(vals_2f)
+# for i in range(data.shape[1]):
+#     axes[1].scatter(times_2f, np.abs(data[:, i]), marker='X', facecolors='none', edgecolors=colors[i], label=f'Component {i}')
+
+# colors = plt.cm.tab10.colors
+
+# # Hairer and Wanner do companion / primary:
+# # > 1 less stiff
+# # < 1 more stiff
+# # I'm going to flip it so larger -> stiffer
+# # > 1 less stiff
+# # < 1 more stiff
+# data_success = np.array(vals_1s) / np.array(vals_2s)
+# if len(vals_etf) > 0:
+#     data_fail = np.array(vals_1f) / np.array(vals_2f)
+
+# for i in range(data_success.shape[1]):
+#     axes[1].scatter(times_1s, np.abs(data_success[:, i]), marker='o', facecolors='none', edgecolors=colors[i], label=f'y{i+1}')
+# if len(vals_etf) > 0:
+#     for i in range(data_fail.shape[1]):
+#         axes[1].scatter(times_1f, np.abs(data_fail[:, i]), marker='X', facecolors='none', edgecolors=colors[i], label=f'y{i+1}')
+
+# axes[1].axhline(1, color="gray", linestyle='--', alpha=0.7)
+
+# legend_elements = []
+
+# # Add component colors
+# for i in range(data_success.shape[1]):
+#     legend_elements.append(Line2D([0], [0], marker='s', color='w', markerfacecolor=colors[i],
+#                                   label=f'y{i+1}'))
+
+# # Create custom legend
+# legend_elements.extend([
+#     Line2D([0], [0], marker='o', color='w', markerfacecolor='none', markeredgecolor='gray', label='Successful step'),
+#     Line2D([0], [0], marker='X', color='w', markerfacecolor='none', markeredgecolor='gray', label='Successful step')])
+
+# axes[1].set_yscale("log")
+# axes[1].legend(handles=legend_elements, loc="best", ncol=2)
+# axes[1].set_xlabel("time")
+# axes[1].grid(True, which="major", linestyle=":", alpha=0.5)
+
+# title = "Absolute Ratio of Error Estimates " + r"($|e_p/e_s|$)"
+# if args.title:
+#     title = args.title + " " + title
+# axes[1].set_title(title)
+# axes[1].grid(True, which="major", linestyle=":", alpha=0.5)
+
+
+colors = plt.cm.tab10.colors
+
+# Hairer and Wanner do companion / primary:
+# > 1 less stiff
+# < 1 more stiff
+# I'm going to flip it so larger -> stiffer
+# > 1 less stiff
+# < 1 more stiff
+data_1 = np.array(vals_1a)
+data_2 = np.array(vals_2a)
+
+for i in range(data_1.shape[1]):
+    axes[1].scatter(times_1a, np.abs(data_1[:, i]), marker='o', facecolors='none', edgecolors=colors[i], label=f'p y{i+1} ')
+for i in range(data_2.shape[1]):
+    axes[1].scatter(times_2a, np.abs(data_2[:, i]), marker='X', facecolors='none', edgecolors=colors[i], label=f's y{i+1}')
+
 axes[1].axhline(1, color="gray", linestyle='--', alpha=0.7)
+
+# legend_elements = []
+
+# # Add component colors
+# for i in range(data_success.shape[1]):
+#     legend_elements.append(Line2D([0], [0], marker='s', color='w', markerfacecolor=colors[i],
+#                                   label=f'y{i+1}'))
+
+# # Create custom legend
+# legend_elements.extend([
+#     Line2D([0], [0], marker='o', color='w', markerfacecolor='none', markeredgecolor='gray', label='Successful step'),
+#     Line2D([0], [0], marker='X', color='w', markerfacecolor='none', markeredgecolor='gray', label='Successful step')])
+
 axes[1].set_yscale("log")
-axes[1].legend(loc="best")
+#axes[1].legend(handles=legend_elements, loc="best", ncol=2)
+axes[1].legend(loc="best", ncol=2)
 axes[1].set_xlabel("time")
+axes[1].grid(True, which="major", linestyle=":", alpha=0.5)
+
+title = "Absolute Ratio of Error Estimates " + r"($|e_p/e_s|$)"
 if args.title:
-    axes[1].set_title(f"{args.title} Local Error Estimates")
-else:
-    axes[1].set_title(f"Local Error Estimates")
+    title = args.title + " " + title
+axes[1].set_title(title)
 axes[1].grid(True, which="major", linestyle=":", alpha=0.5)
 
 

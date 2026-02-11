e52eda41c903849dfd88cb34bb2b28b7c4e4f3c6
Daniel R. Reynolds <dreynolds@umbc.edu>
2026-02-11 09:02:30 -0500

    Fixed locations for stage index updates in SSP(4,3)

--- src/arkode/arkode_lsrkstep.c
+++ src/arkode/arkode_lsrkstep.c
@@ -1933,6 +1933,7 @@ int lsrkStep_TakeStepSSP43(ARKodeMem ark_mem, sunrealtype* dsmPtr, int* nflagPtr
   SUNLogInfo(ARK_LOGGER, "end-stages-list", "status = success");
 
   /* Perform the second stage, and accumulate embedding into tempv1 */
+  step_mem->istage = 1;
   ark_mem->tcur = ark_mem->tn + hp5;
   SUNLogInfo(ARK_LOGGER, "begin-stages-list",
              "stage = %i, tcur = " SUN_FORMAT_G, 1, ark_mem->tcur);
@@ -1954,9 +1955,6 @@ int lsrkStep_TakeStepSSP43(ARKodeMem ark_mem, sunrealtype* dsmPtr, int* nflagPtr
       return ARK_POSTPROCESS_STAGE_FAIL;
     }
   }
-
-  /* update stage index */
-  step_mem->istage = 1;
 
   /* apply user-supplied stage preprocessing function (if supplied) */
   if (ark_mem->PreProcessRHS != NULL)
@@ -1986,6 +1984,7 @@ int lsrkStep_TakeStepSSP43(ARKodeMem ark_mem, sunrealtype* dsmPtr, int* nflagPtr
   SUNLogInfo(ARK_LOGGER, "end-stages-list", "status = success");
 
   /* Perform the third stage */
+  step_mem->istage = 2;
   ark_mem->tcur = ark_mem->tn + ark_mem->h;
   SUNLogInfo(ARK_LOGGER, "begin-stages-list",
              "stage = %i, tcur = " SUN_FORMAT_G, 2, ark_mem->tcur);
@@ -2007,9 +2006,6 @@ int lsrkStep_TakeStepSSP43(ARKodeMem ark_mem, sunrealtype* dsmPtr, int* nflagPtr
       return ARK_POSTPROCESS_STAGE_FAIL;
     }
   }
-
-  /* update stage index */
-  step_mem->istage = 2;
 
   /* Evaluate stage RHS */
 
@@ -2040,6 +2036,7 @@ int lsrkStep_TakeStepSSP43(ARKodeMem ark_mem, sunrealtype* dsmPtr, int* nflagPtr
   SUNLogInfo(ARK_LOGGER, "end-stages-list", "status = success");
 
   /* Perform the fourth stage */
+  step_mem->istage = 3;
   ark_mem->tcur = ark_mem->tn + hp5;
   SUNLogInfo(ARK_LOGGER, "begin-stages-list",
              "stage = %i, tcur = " SUN_FORMAT_G, 3, ark_mem->tcur);
@@ -2073,9 +2070,6 @@ int lsrkStep_TakeStepSSP43(ARKodeMem ark_mem, sunrealtype* dsmPtr, int* nflagPtr
       return ARK_POSTPROCESS_STAGE_FAIL;
     }
   }
-
-  /* update stage index */
-  step_mem->istage = 3;
 
   /* Evaluate stage RHS */
 
